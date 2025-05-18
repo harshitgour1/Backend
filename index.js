6bdef1c291344bb6a5e62c24c00a71947a820c60
Harshit Goud <harshitgour6261@gmail.com>
2025-05-18 20:26:07 +0530

    github data added

--- index.js
+++ index.js
@@ -22,3 +22,43 @@ app.get('/youtube', (req, res)=>{
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+const githubData = {
+  "login": "harshitgour1",
+  "id": 161942589,
+  "node_id": "U_kgDOCacMPQ",
+  "avatar_url": "https://avatars.githubusercontent.com/u/161942589?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/harshitgour1",
+  "html_url": "https://github.com/harshitgour1",
+  "followers_url": "https://api.github.com/users/harshitgour1/followers",
+  "following_url": "https://api.github.com/users/harshitgour1/following{/other_user}",
+  "gists_url": "https://api.github.com/users/harshitgour1/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/harshitgour1/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/harshitgour1/subscriptions",
+  "organizations_url": "https://api.github.com/users/harshitgour1/orgs",
+  "repos_url": "https://api.github.com/users/harshitgour1/repos",
+  "events_url": "https://api.github.com/users/harshitgour1/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/harshitgour1/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Harshit Goud",
+  "company": null,
+  "blog": "",
+  "location": "Indore, India ",
+  "email": null,
+  "hireable": true,
+  "bio": "An Ai Enthusiast and Researcher.. \r\nFrontEnd Developer Currently Learning Backend Technologies and GEN-AI...",
+  "twitter_username": null,
+  "public_repos": 8,
+  "public_gists": 0,
+  "followers": 1,
+  "following": 2,
+  "created_at": "2024-03-02T12:01:33Z",
+  "updated_at": "2025-05-11T05:09:43Z"
+}
+
+app.get('/github', (req, res)=> {
+    res.json(githubData)
+})
